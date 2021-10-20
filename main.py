17cb94e93f50f7bf99b1491f6a4288339f06c43f
olegpecheneg <92542075+olegpecheneg@users.noreply.github.com>
2021-10-21 00:57:52 +0300

    RABOTAET

---
+++ main.py
@@ -0,0 +1,24 @@
+from decimal import Decimal as de
+from decimal import *
+
+n = int(input())
+getcontext().prec = n + 1
+
+def pi(n):
+    pi = de(13591409)
+    ak = de(1)
+    k = 1
+    while k < n:
+        ak *= -de((6 * k - 5) * (2 * k - 1) * (6 * k - 1)) / de(k * k * k * 26680 * 640320 * 640320)
+
+        val = ak * (13591409 + 545140134 * k)
+
+        d = de((6 * k - 5) * (2 * k - 1) * (6 * k - 1)) / de(k * k * k * 26680 * 640320 * 640320)
+
+        pi += val
+        k += 1
+    pi = pi * de(10005).sqrt() / 4270934400
+    pi = pi ** (-1)
+    return pi
+
+print(pi(n))
